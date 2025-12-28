af2f95e1040f08f28b9becada9e680bbcfc14bc9
Mansur <vakhabov.mans@gmail.com>
2025-12-28 07:54:17 +0300

    finish createTask

--- http/dto.go
+++ http/dto.go
@@ -4,11 +4,19 @@ import (
 	"encoding/json"
 	"fmt"
 	"time"
+
+	"github.com/go-playground/validator/v10"
 )
 
 type TaskDTO struct {
-	Title string
-	Description string
+	Title       string `json:"title" validate:"required,min=1"`
+	Description string `json:"description" validate:"required,min=1"`
+}
+
+var validate = validator.New()
+
+func (t TaskDTO) ValidateForCreate() error {
+	return validate.Struct(t)
 }
 
 type ErrorDTO struct {
--- http/handlers.go
+++ http/handlers.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"encoding/json"
+	"errors"
 	"github/m4nsur/todo-api-lrn/todo"
 	"net/http"
 	"time"
@@ -27,6 +28,37 @@ func (h *HTTPHandlers) HandleCreateTask(w http.ResponseWriter, r *http.Request) 
 		http.Error(w, errDTO.ToString(), http.StatusBadRequest)
 		return 
 	}
+
+	if err := taskDTO.ValidateForCreate(); err != nil {
+		errDTO := ErrorDTO{
+			Message: err.Error(),
+			Time: time.Now(),
+		}
+		http.Error(w, errDTO.ToString(), http.StatusBadRequest)
+		return
+	}
+
+	todoTask := todo.NewTask(taskDTO.Title, taskDTO.Description); 
+	if err := h.todoList.AddTask(todoTask); err != nil {
+		errDTO := ErrorDTO{
+			Message: err.Error(),
+			Time: time.Now(),
+		}
+		if errors.Is(err, todo.ErrTaskAlreadyExists) {
+			http.Error(w, errDTO.ToString(), http.StatusConflict)
+		} else {
+			http.Error(w, errDTO.ToString(), http.StatusConflict)
+		}
+		return
+	} 
+
+	b, err := json.MarshalIndent(todoTask, "", "    ")
+	if err != nil {
+		panic(err)
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	w.Write(b)
 }
 
 func (h *HTTPHandlers) HandleGetTask(w http.ResponseWriter, r *http.Request) {
